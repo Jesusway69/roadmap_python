6cd1d13512511e57c0ffb83330da15a171f64a6e
JesusWay69 HP elitebook <jesusway69@gmail.com>
2024-02-21 23:09:56 +0100

    20240221 Continuación ejercicio 07

--- #07/jesusway69.py
+++ #07/jesusway69.py
@@ -1,6 +1,6 @@
 import os
 os.system('cls')
-from collections import deque
+import collections
 
 """
  * EJERCICIO:
@@ -8,6 +8,45 @@ from collections import deque
  * pilas (stacks - LIFO) y las colas (queue - FIFO) utilizando una estructura de array
  * o lista (dependiendo de las posibilidades de tu lenguaje).
  *
+"""
+
+
+#MÉTODO PILA (LIFO)
+def pila (my_list):
+ for i in range (1,11):# Generamos números del 1 al 10
+    my_list.append(i)# Los añadimos a la lista
+ print(my_list)# Imprimimos la lista con los 10 números
+ eliminado = my_list.pop()# El método pop elimina el último elemento de la lista si no le pasamos ningún índice
+ print("Eliminamos el número", eliminado)# Mostramos el número eliminado
+ print(my_list,"\n")# imprimimos la lista con el último elemento eliminado
+
+
+#MÉTODO COLA (FIFO)
+def cola (my_list):
+  for i in range (1,11):# Generamos números del 1 al 10
+    my_list.append(i)# Los añadimos a la lista
+  print(my_list)# Imprimimos la lista con los 10 números
+  eliminado = my_list.pop(0)# El método pop elimina el elemento de la lista cuyo índice le pasemos
+  print("Eliminamos el número", eliminado)# Mostramos el número eliminado
+  print(my_list,"\n")# imprimimos la lista con el primer elemento eliminado
+
+#MËTODO COLA (FIFO) CON MÉTODO DEQUE
+def cola_deque (my_list):
+  my_list = collections.deque(range(1,11))# Generamos y añadimos números del 1 al 10
+  print(my_list)# Imprimimos la lista con los 10 números
+  eliminado = my_list.popleft()# El método popleft elimina el primer elemento de la lista (no admite argumentos)
+  print("Eliminamos el número", eliminado)# Mostramos el número eliminado
+  print(my_list,"\n")# imprimimos la lista con el primer elemento eliminado
+
+my_list = []
+pila(my_list)
+my_list = []
+cola(my_list)
+my_list = []
+cola_deque(my_list)
+
+
+"""
  * DIFICULTAD EXTRA (opcional):
  * - Utilizando la implementación de pila y cadenas de texto, simula el mecanismo adelante/atrás
  *   de un navegador web. Crea un programa en el que puedas navegar a una página o indicarle
@@ -20,41 +59,19 @@ from collections import deque
  *   interpretan como nombres de documentos.
 """
 
+def browser(my_list: list):
+ 
+ while len(my_list)>=1:
+   print("Estás en la web:",my_list[len(my_list)-1])
+   order = input("Introduzca web o escriba back para retroceder:")
+   print()
+   if order == "back":
+     my_list.pop()
+   else:
+     my_list.append(order)
 
+my_list = ["inicio.com"]
+browser(my_list)
 
-def pila (my_list):
- for i in range (1,11):
-    my_list.append(i)
-
- print(my_list)
-
- my_list.pop()
-
- print(my_list)
-
-
-
-def cola (my_list):
-  for i in range (1,11):
-    my_list.insert(0,i)
-  print(my_list)
-  my_list.pop(0)
-  print(my_list)
-
-def cola2 (my_list):
-  my_list = deque(my_list)
-  for i in range (1,11):
-    my_list.append(i)
-  print(my_list)
-  my_list.popleft
-  print(my_list)
+def printer ():
   
-  
-
-
-my_list = []
-pila(my_list)
-my_list = []
-cola(my_list)
-my_list = []
-cola2(my_list)
