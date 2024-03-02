547b47605e6b35ba3c4cef94146f33330225be7f
Jesusway69 Mac mini macOS <jesusway69@gmail.com>
2024-03-02 20:25:49 +0100

    20240302 Ejercicio 09 terminado Python

--- #09/jesusway69_2.py
+++ #09/jesusway69_2.py
@@ -12,69 +12,55 @@ os.system('clear') #MAC/LINUX
  * actividad, y almacenan los empleados a su cargo."""
 
 class Empleado():
-    
-    
+ 
     def __init__(self):
        self.empleados_gerente = empleados_gerente = []
        self.empleados_proyectos = empleados_proyectos = []
-       self.finanzas = False
-       self.compras = False
-       self.proyectos = False
-       self.organizacion = False
-       self.programacion = False
-       self.despliegue = False
+       self.finanzas = "NO"
+       self.compras = "NO"
+       self.proyectos = "NO"
+       self.organizacion = "NO"
+       self.programacion = "NO"
+       self.despliegue = "NO"
    
     def print(self, cargo, id , nombre):
           self.cargo = cargo
           self.id = id
           self.nombre = nombre
-        #   if self.cargo == "Programador" or self.cargo =="Gerente de proyecto":
-        #       self.empleados_gerente.append(nombre)
-              
-        #   if self.cargo == "Programador":
-        #       self.empleados_proyectos.append(nombre)
          
           print ("\nid:", self.id, "\nNombre:",self.nombre,"\nCargo:", self.cargo,"\n¿Hace finanzas? =", 
-               self.finanzas,"\n¿Hace compras? =", self.compras, "\n¿Hace proyectos? =", self.proyectos,
+               self.finanzas,"\n¿Hace compras? =", self.compras, "\n¿Planifica proyectos? =", self.proyectos,
                "\n¿Organiza el trabajo? =", self.organizacion, "\n¿Pica código? =", self.programacion,
                "\n¿Despliega programas? =",self.despliegue)
       
     def print_empleados(self,list_empleados):
-        print ("\nEmpleados a su cargo:", end=' ')
-        [print(i, end=' ')for i in list_empleados]
-
-          
-        
-
-          
+        print ("Empleados a su cargo:", end=' ')
+        print(' , '.join(list_empleados))
+        print("")
+     
 class Gerente(Empleado):
       
       def es_gerente(self, id, nombre,lista_g):
-        self.finanzas=True
-        self.compras=True
+        self.finanzas="SI"
+        self.compras="SI"
         self.print("Gerente", id ,nombre)
         self.print_empleados(lista_g)
 
-
  
-     
 class Gerente_proyecto(Gerente,Empleado):
          
       def es_gerente_proyecto(self, id, nombre,lista_p):
-        self.organizacion=True
-        self.proyectos=True
+        self.organizacion="SI"
+        self.proyectos="SI"
         self.print("Gerente de proyecto", id ,nombre)
         self.print_empleados(lista_p)
-        
-
 
 class Programador (Gerente_proyecto,Gerente,Empleado):
       
       def es_programador(self, id, nombre):
-        self.programacion=True
-        self.despliegue=True     
+        self.programacion="SI"
+        self.despliegue="SI"     
         self.print("Programador", id ,nombre)
-        
         
 
 gerente = Gerente()
@@ -82,20 +68,24 @@ gerente_proyecto = Gerente_proyecto()
 programador = Programador()
 
 dict_empleados = {
-1: "Paco",
+1: "Miguel",
 2: "Sandra",
 3: "Carlos",
-4: "Ana",
+4: "Rocío",
 5: "Pedro",
-6: "Lucía"
+6: "Lucía",
+7: "Steven",
+8: "Sara",
+9: "Guillermo",
+10: "Leire"
 
 }
 list_empleados = []
 list_proyectos = []
 for k,v in dict_empleados.items():
-      if  k>2 and k<5:
-          list_proyectos.append(v)
-      if k>= 5:
+      if  k>2 and k<6:
+          list_empleados.append(v)
+      if k>= 6:
           list_empleados.append(v)
           list_proyectos.append(v)
 
@@ -104,12 +94,10 @@ for k,v in dict_empleados.items():
    if k<=2:
         gerente.es_gerente(k,v,list_empleados)
         
-   elif k>2 and k<5:
-        
+   elif k>2 and k<6:
         gerente_proyecto.es_gerente_proyecto(k,v,list_proyectos)
         
    else:
-       
         programador.es_programador(k,v)
        
 
@@ -117,9 +105,6 @@ for k,v in dict_empleados.items():
       
 
 
-# gerente.es_gerente(1,"Paco")
-# gerente_proyecto.es_gerente_proyecto(2, "Luis")
-# programador.es_programador(3,"Pepe")
 
 
 
