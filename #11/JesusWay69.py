ebb2bca6d323b925ef971b084dbeb3dd685d47aa
JesusWay69 HP elitebook <jesusway69@gmail.com>
2024-03-13 21:42:35 +0100

    20240313 continuación ejercicio 11 Python

--- #11/JesusWay69.py
+++ #11/JesusWay69.py
@@ -1,5 +1,5 @@
 import os
-os.system('clear')
+os.system('cls')
 
 
 """
@@ -20,30 +20,31 @@ os.system('clear')
 
 
 
-file=open("JesusWay69.txt", "w")
-file.write("Jesus\n")
-file.write("99\n")
-file.write("Python\n")
-file.close()
-file=open("JesusWay69.txt", "r")
-readfile = file.read()
-file.close
-print(readfile)
+# file=open("JesusWay69.txt", "w")
+# file.write("Jesus\n")
+# file.write("99\n")
+# file.write("Python\n")
+# file.close()
+# file=open("JesusWay69.txt", "r")
+# readfile = file.read()
+# file.close
+# print(readfile)
 
-file = open("JesusWay69.txt", "a")
-file.write("linea nueva añadida\n")
-file=open("JesusWay69.txt", "r")
-readfile = file.read()
-file.close
-print(readfile)
+# file = open("JesusWay69.txt", "a")
+# file.write("linea nueva añadida\n")
+# file=open("JesusWay69.txt", "r")
+# readfile = file.read()
+# file.close
+# print(readfile)
 
-file = open("JesusWay69.txt", "r+")
-readfile = file.read()
-file.write("otra linea nueva añadida\n")
+# file = open("JesusWay69.txt", "r+")
+# readfile = file.read()
+# file.write("otra linea nueva añadida\n")
 
-readfile = file.read()
-file.close
-print(readfile , "hola")
+# readfile = file.read()
+# file.close
+# print(readfile)
+# print("\n")
 
 
 
@@ -64,25 +65,90 @@ print(readfile , "hola")
 class Devices ():
    def __init__(self,nombre_fichero):
     self.nombre_fichero = nombre_fichero
-    open (self.nombre_fichero, "w").close() 
+    file = open (self.nombre_fichero, "w")
+    file.write("PRODUCTO , UDS VENDIDAS, PRECIO\n")
+    file.close()
    
-   def añadir(self, producto, cantidad, precio):
+   def add(self, producto, cantidad, precio):
       file = self.nombre_fichero
       self.producto = producto
       self.cantidad = cantidad
       self.precio = precio
       file = open(file,"a") 
+      
       file.write(self.producto)
-      file.write(",")
+      file.write(" , ")
       file.write(self.cantidad)
-      file.write(",")
+      file.write(" , ")
       file.write(self.precio)
       file.write("\n")
       file.close
+
+   def show_all(self):
+     file = self.nombre_fichero
+     n_line = 0
+     file = open(file, "r")
+     
+     print("Contenido del fichero:\n")
+     for i in file:
+      if n_line == 0:
+        print(i)
+        n_line +=1
+      else:
+       print(n_line, " - " ,i)
+       n_line +=1
+     file.close()
    
-os.remove("ventas.txt")
-# device = Devices("ventas.txt")
-# device.añadir("Macbook", "7", "2500")
-# device.añadir("HP Elitebook", "5", "1500")
+   def find(self,substring):
+     file = self.nombre_fichero
+     self.substring = substring
+     product_list =[]
+     file =  open(file, 'r+')
+     line = file.readline()
+     for line in file:
+         if self.substring in line:
+           product_list.append(line)
+           continue
+         elif self.substring not in line:
+           continue
+         else:
+           print("Artículo no encontrado\n")
+     print("los productos que busca son:\n")
+     for i in product_list:
+       print("- ",i)
+     file.close()
+
+   def remove(self):
+      file = self.nombre_fichero
+      self.show_all()
+      line_edit = int(input("Introduzca el número del elemento a borrar: "))
+      with open(file , "r+") as file:
+       lines = file.readlines()
+       print(lines)
+      #with open(file, "w") as file:
+       line = lines[line_edit]
+       lines.remove(line)
+       for line in lines:
+         file.write(line)
+      
+       file1 = file.read()
+       print(file1)
+       
+      
+
+       
+
+
+   
+#os.remove("ventas.txt")
+device = Devices("ventas.txt")
+device.add("Macbook", "7", "2500")
+device.add("HP Elitebook", "5", "1500")
+device.add("Samsung Galaxy S24", "9", "700")
+device.add("HP Pavilion", "6", "1200")
+#device.find("0")
+#device.show_all()
+device.remove()
+
 
       
