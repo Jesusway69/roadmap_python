41096637146c93226bea3594220caeda4c964443
JesusWay69 HP elitebook <jesusway69@gmail.com>
2024-03-19 00:22:35 +0100

    20240318 Ejercicio 11 casi terminado Python

--- #11/JesusWay69.py
+++ #11/JesusWay69.py
@@ -62,93 +62,175 @@ os.system('cls')
 
 """
 
-class Devices ():
-   def __init__(self,nombre_fichero):
-    self.nombre_fichero = nombre_fichero
-    file = open (self.nombre_fichero, "w")
-    file.write("PRODUCTO , UDS VENDIDAS, PRECIO\n")
-    file.close()
+def create(file):
+    try:
+        file = open (file, "x")
+        file.write("PRODUCTO , UDS VENDIDAS, PRECIO\n")
+        file.close()
+    except:
+        print(f"El fichero \"{file}\" ya existe")
    
-   def add(self, producto, cantidad, precio):
-      file = self.nombre_fichero
-      self.producto = producto
-      self.cantidad = cantidad
-      self.precio = precio
-      file = open(file,"a") 
-      
-      file.write(self.producto)
-      file.write(" , ")
-      file.write(self.cantidad)
-      file.write(" , ")
-      file.write(self.precio)
-      file.write("\n")
-      file.close
+   
+def add(file,producto, cantidad, precio):
+    file = open(file,"a")
+    file.write(f"{producto}, {cantidad}, {precio}\n")
+    file.close
 
-   def show_all(self):
-     file = self.nombre_fichero
-     n_line = 0
+def show_all(file): 
      file = open(file, "r")
+     content = file.read()
      
-     print("Contenido del fichero:\n")
-     for i in file:
-      if n_line == 0:
-        print(i)
-        n_line +=1
-      else:
-       print(n_line, " - " ,i)
-       n_line +=1
-     file.close()
-   
-   def find(self,substring):
-     file = self.nombre_fichero
-     self.substring = substring
-     product_list =[]
-     file =  open(file, 'r+')
-     line = file.readline()
-     for line in file:
-         if self.substring in line:
-           product_list.append(line)
-           continue
-         elif self.substring not in line:
-           continue
-         else:
-           print("Artículo no encontrado\n")
-     print("los productos que busca son:\n")
-     for i in product_list:
-       print("- ",i)
-     file.close()
+     print(content)
 
-   def remove(self):
-      file = self.nombre_fichero
-      self.show_all()
-      line_edit = int(input("Introduzca el número del elemento a borrar: "))
-      with open(file , "r+") as file:
-       lines = file.readlines()
-       print(lines)
-      #with open(file, "w") as file:
-       line = lines[line_edit]
-       lines.remove(line)
-       for line in lines:
-         file.write(line)
-      
-       file1 = file.read()
-       print(file1)
-       
-      
+def find(file, substring:str):
+    if not substring.isdigit():
+        product_list =[]
+        line_num = 0
+        file =  open(file, 'r+')
+        line = file.readline()
+        for line in file:
+            line_num +=1
+            if substring in line:
+                line = str(line_num) + "- " + line
+                product_list.append(line)
+                continue
+            elif substring not in line:
+                continue
+            else:
+                print("Artículo no encontrado\n")
+        print(f"Productos encontrados que contienen el nombre \"{substring}\":\n")
+        for i in product_list:
+            print(i)
+        file.close()
+    else:
+        print("La búsqueda de un producto no se puede hacer con valores numéricos únicamente")
 
-       
+print ("""
+Elija una opción:
+1-Crear nuevo archivo
+2-Mostrar contenido de un archivo por consola
+3-Añadir producto
+4-Editar producto
+5-Calcular ventas totales 
+6-Calcular ventas por producto
+7-Eliminar producto
+8-Eliminar archivo y salir
+ 
+       """)
+option = input("Introduzca una opción del 1 al 7: ")
+if option == "1":
+    file = input ("Escriba el nombre del archivo que desea crear con su extensión: ")
+    create(file)
+    print(f"El fichero \"{file}\" se ha creado correctamente")
 
+if option == "2":
+    while True:
+        file = input("Introduzca el nombre del fichero cuyo contenido quiera ver: ")
+        if not os.path.isfile(file):
+            print(f"El archivo {file} no existe")
+        else:
+            print(f"\nContenido del fichero \"{file}\":\n")
+        show_all(file)
+        break
 
-   
-#os.remove("ventas.txt")
-device = Devices("ventas.txt")
-device.add("Macbook", "7", "2500")
-device.add("HP Elitebook", "5", "1500")
-device.add("Samsung Galaxy S24", "9", "700")
-device.add("HP Pavilion", "6", "1200")
-#device.find("0")
-#device.show_all()
-device.remove()
+if option == "3":
+ while True:
+    file = input("Introduzca el nombre del fichero donde añadir datos: ")
+    if not os.path.isfile(file):
+        print (f"El archivo {file} no existe")
+        continue
+        
+    product = input("Introduzca el nombre del producto: ").title()
+    sales = input("Introduzca la cantidad de uds vendidas: ")
+    price = input("Introduzca el precio por unidad: ")
+    if not sales.isdigit() or not price.isdigit():
+        print("ERROR: los campos 'ventas' y 'precio' deben ser valores numéricos, intente de nuevo")
+    else:
+    
+        add(file, product, sales, price)
+        print(f"El producto \"{product}\" con {sales} uds vendidas y valor de {price}€/ud se ha añadido al fichero {file}")
+        break
 
+if option == "4":
+    while True:
+        file = input ("Escriba el nombre del fichero donde buscar: ")
+        if not os.path.isfile(file):
+            print (f"El archivo {file} no existe")
+            continue
+        product = input("Escriba la marca y/o el modelo del producto que busca: ").title()
+        find(file, product)
+        line = int(input("Introduzca el número del producto que desea editar: "))
+        column =int(input("Introduzca el número del campo que desea editar, 1-Producto , 2-Uds vendidas o 3-Precio: "))
+        new_data = input("Introduzca el nuevo dato: ")
+        content = []
+        with open(file, 'r+') as file1:
+            content = file1.readlines()
+            columns = content[line].split(", ")
+            columns[column-1] = new_data
+            content[line] = ", ".join(columns)+"\n"
+        with open(file, 'w') as file1:
+         file1.writelines(content)
+        break
 
-      
+if option == "5":
+    file = input ("Escriba el nombre del fichero del cual desee calcular las ventas totales: ")
+    with open(file, 'r+') as file:
+            content = file.readlines()
+            acc = 0
+            for i in range (1,(len(content))):
+                element = content[i]
+                column = element.split(", ")
+                sales = int(column[1])
+                acc += sales
+            print(f"Las uds totales vendidas de todos los productos es: {acc}")  
+
+if option == "6":
+    pass
+
+if option == "7":
+    while True:
+        file = input ("Escriba el nombre del fichero donde buscar: ")
+        if not os.path.isfile(file):
+            print (f"El archivo {file} no existe")
+            continue
+        product = input("Escriba la marca y/o el modelo del producto que busca: ").title()
+        find(file, product)
+        try:
+           line = int(input("Introduzca el número del producto que desea borrar: "))
+        except:
+            print("Sólo se pueden introducír números")
+            continue
+     
+        file1 = open(file , "r+")
+        lines = file1.readlines()
+        if line <0 or line > len(lines):
+            print(f"el número {line} no pertenece a ningún producto")
+        else:
+           line = lines[line]
+           lines.remove(line)
+           line.replace("\\n", "")
+           print(f"El artículo {line} ha sido eliminado")
+        file1.close()
+        file1 = open (file, "w")
+        for line in lines:
+            file1.write(line)
+        break
+
+if option == "8":
+    while True:
+        file = input ("Escriba el nombre del fichero a eliminar o exit para salir sin borrar: ")
+        if os.path.isfile(file):
+            confirmed = input(f"""¿Seguro que quiere eliminar el fichero \"{file}\"? 
+  pulse enter para confirmar o escriba cualquier cosa para salir sin borrar: """)
+            if confirmed == "":
+                os.remove(file)
+                print(f"El fichero {file} se ha eliminado con éxito, fin del programa.")
+                break
+            else:
+                print("Fin del programa.") 
+                break
+        elif file == "exit":
+            print("Fin del programa.")
+            break
+        else:
+            print(f"El fichero {file} no existe, pruebe de nuevo\n")
---
+++ #11/JesusWay69_2.py
@@ -0,0 +1,125 @@
+import os
+os.system('cls')
+
+
+"""
+* DIFICULTAD EXTRA (opcional):
+ * Desarrolla un programa de gestión de ventas que almacena sus datos en un 
+ * archivo .txt.
+ * - Cada producto se guarda en una línea del arhivo de la siguiente manera:
+ *   [nombre_producto], [cantidad_vendida], [precio].
+ * - Siguiendo ese formato, y mediante terminal, debe permitir añadir, consultar,
+ *   actualizar, eliminar productos y salir.
+ * - También debe poseer opciones para calcular la venta total y por producto.
+ * - La opción salir borra el .txt. 
+
+"""
+
+
+
+# class Devices ():
+#    def __init__(self,nombre_fichero):
+#     self.nombre_fichero = nombre_fichero
+#     file = open (self.nombre_fichero, "w")
+#     file.write("PRODUCTO , UDS VENDIDAS, PRECIO\n")
+#     file.close()
+   
+#    def add(self, producto, cantidad, precio):
+#       file = self.nombre_fichero
+#       self.producto = producto
+#       self.cantidad = cantidad
+#       self.precio = precio
+#       file = open(file,"a") 
+      
+#       file.write(self.producto)
+#       file.write(" , ")
+#       file.write(self.cantidad)
+#       file.write(" , ")
+#       file.write(self.precio)
+#       file.write("\n")
+#       file.close
+
+#    def show_all(self):
+#      file = self.nombre_fichero
+#      n_line = 0
+#      file = open(file, "r")
+     
+#      print("Contenido del fichero:\n")
+#      for i in file:
+#       if n_line == 0:
+#         print(i)
+#         n_line +=1
+#       else:
+#        print(n_line, " - " ,i)
+#        n_line +=1
+#      file.close()
+   
+#    def find(self,substring):
+#      file = self.nombre_fichero
+#      self.substring = substring
+#      product_list =[]
+#      file =  open(file, 'r+')
+#      line = file.readline()
+#      for line in file:
+#          if self.substring in line:
+#            product_list.append(line)
+#            continue
+#          elif self.substring not in line:
+#            continue
+#          else:
+#            print("Artículo no encontrado\n")
+#      print("los productos que busca son:\n")
+#      for i in product_list:
+#        print("- ",i)
+#      file.close()
+
+#    def remove(self):
+#       file = self.nombre_fichero
+#       self.show_all()
+#       line_edit = int(input("Introduzca el número del elemento a borrar: "))
+#       with open(file , "r+") as file:
+#        lines = file.readlines()
+#        print(lines)
+#       #with open(file, "w") as file:
+#        line = lines[line_edit]
+#        lines.remove(line)
+#        for line in lines:
+#          file.write(line)
+      
+#        file1 = file.read()
+#        print(file1)
+       
+      
+
+       
+
+
+   
+# #os.remove("ventas.txt")
+# device = Devices("ventas.txt")
+# device.add("Macbook", "7", "2500")
+# device.add("HP Elitebook", "5", "1500")
+# device.add("Samsung Galaxy S24", "9", "700")
+# device.add("HP Pavilion", "6", "1200")
+# #device.find("0")
+# #device.show_all()
+# device.remove()
+
+
+      
+
+            
+                                                          
+
+
+
+
+
+
+
+
+
+
+
+
+
