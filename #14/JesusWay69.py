4e131d3986a4e4363331eb552bdf1cbac0a31dde
JesusWay69 HP elitebook <jesusway69@gmail.com>
2024-04-07 21:45:43 +0200

    20240407 Ejercicio 14 terminado Python

--- #14/JesusWay69.py
+++ #14/JesusWay69.py
@@ -27,25 +27,29 @@ from datetime import timezone as TZ
 
 week_days = ["Lunes","Martes","Miércoles","Jueves","Viernes","Sábado","Domingo"]
 months =["Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre"]
-utc_spain_timezone = TZ(TD(hours=+2))
 current_datetime = DT.now()#Con .now sobre datetime generamos un objeto datetime con la fecha y hora actual del sistema
-current_date = D.today()#Con .today sobre date generamos un objeto date con la fecha actual del sistema
+utc_spain_timezone = TZ(TD(hours=+2))# Con el módulo timezone podemos definir una zona horaria uct concreta
+sillicon_valley_time = current_datetime.astimezone(TZ(TD(hours=-7)))#Con .astimezone le pasamos el objeto timezone con la diferencia horaria definida
+                                                                    # en un timedelta 
+current_date = D.today()#Con .today sobre date generamos un objeto date con la fecha actual del sistema (similar a .now sobre datetime)
 format_current_date = D.today().strftime('%A, %d-%B-%Y')#Con .strftime sobre date generamos un string formateado "a la carta"
 format_manual_time = DT.strptime('20-05-1998 22:15:30', '%d-%m-%Y %H:%M:%S')#Con .strptime generamos un objeto datetime a partir de un string
 format_manual_time2 = 'Formateo desde datetime: {}/{}/{}'.format(DT.now().day,DT.now().month,DT.now().year)
 format_manual_time3 = 'Formateo desde date: {}/{}/{}'.format(D.today().day,D.today().month,D.today().year)
 #Con .format a partir de una combinación de objetos datetime o date generamos la fecha, hora o ambas en string
 week_day = week_days[format_manual_time.weekday()] #Con .weekday nos devuelve un número que corresponde al día de la semana donde 1 es el lunes..
-current_week_day = week_days[current_date.weekday()]
-sillicon_valley_time = current_datetime.astimezone(TZ(TD(hours=-7)))
-current_year = current_date.year
-current_month = months[current_date.month-1]
+current_week_day = week_days[current_date.weekday()]# Aplicando ese número como índice de una lista con los días podemos generar el día en texto
+
+current_year = current_date.year #Cada objeto date nos permite acceder indivudualmente a cada componente de la fecha
+current_month = months[current_date.month-1]# Como cada componente lo devuelve en un int podemos usarlo como índice de una lista
 current_day = current_date.day
-current_hour = current_datetime.hour
+current_hour = current_datetime.hour# Con los objetos datetime tambien podemos acceder a cada componente de fecha y hora
 current_minute = current_datetime.minute
 current_second = current_datetime.second
-my_age = current_datetime - format_manual_time
-my_age_years = int(my_age.days//365.25)
+my_age = current_datetime - format_manual_time#Podemos operar con 2 objetos datetime que nos devuelve la operación en un objeto timedelta
+my_age_years = int(my_age.days//365.25)# El timedelta nos devuelve un objeto con el total del intervalo temporal en días, horas,minutos y segundos con decimales
+#Los objetos timedelta tambien proveen de métodos para separar solo los días (.days) o la franja sobrante de tiempo en segundos (.seconds)
+
 
 print("Fecha y hora en España:",current_datetime,"\n",
     "Fecha y hora en Sillicon Valley (USA):", sillicon_valley_time,"\n",
@@ -57,11 +61,13 @@ print("Fecha y hora en España:",current_datetime,"\n",
         format_manual_time2 , "\n",
         format_manual_time3 , "\n",
         my_age_years, "años\n",
+        "Objeto timedelta completo: ",my_age, '\n',
         my_age.days, "días\n",
+        
         "Zona horaria España:", utc_spain_timezone , "\n",        
         "\n\n\n"
         )
-
+os.system('pause')
 
 
 
@@ -81,7 +87,7 @@ def input_data()->D:
             continue
         while True:
             month = input("Escriba el mes de nacimiento: ")
-            if  not month.isdigit   or len(month)>2 or len(month)<=0:
+            if  not month.isdigit or len(month)>2 or len(month)<=0:
                 print ("ERROR: El mes debe ser numérico de 1 o 2 cifras")
                 continue
             elif int(month)<1 or int(month)>12:
@@ -110,21 +116,21 @@ def show_data(date_object:D):
     days_passed = D.today() - beginning_year
     weeks_passed = days_passed.days//7+1
 
-    print(f"\nHoy es {current_week_day} día {current_day} de {current_month} de {current_year}")
-    print(f"Y son las {DT.now().hour} horas y {current_minute} minutos\n")
+    print(f"\nHoy es {current_week_day} día {DT.now().day} de {current_month} de {DT.now().year}")
+    print(f"Y son las {DT.now().hour} horas y {DT.now().minute} minutos\n")
     print(f"Han transcurrido {weeks_passed} semanas y estamos en el día {days_passed.days+1} de {D.today().year}\n")
-    if DT.now().hour in range(6,11):
+    if DT.now().hour>=6 and DT.now().hour<12:
       print("¡¡Buenos días!!\n")
-    elif DT.now().hour in range(12,21):
+    elif DT.now().hour>=12 and DT.now().hour<=20:
         print("¡¡Buenas tardes!!\n")
     else:
         print("¡¡Buenas noches!!\n")
 
     print(f"Como has indicado que naciste el día {date_object.day} de {months[date_object.month-1]} de {date_object.year} ...\n")
     print("... (por cierto naciste un", week_days[format_date.weekday()],")\n")
-    if current_day == date_object.day and current_date.month == date_object.month:
+    if DT.now().day == date_object.day and DT.now().month == date_object.month:
             print(f"¡¡¡FELICIDADES!!! hoy cumples {my_age_years} años, casi nada\n")
     else:
-            print(f"y por lo tanto tienes {my_age_years} años")
+            print(f"...tienes {my_age_years} años")
 
 show_data(input_data())
