d51f12e2a6dce35e454b39e646455f5f6011b362
JesusWay69 HP elitebook <jesusway69@gmail.com>
2024-04-22 20:38:46 +0200

    20240422 Ejercicio 16 terminado Python, añadidas anotaciones y pequeñas correcciones en Java

--- #16/JesusWay69.java
+++ #16/JesusWay69.java
@@ -29,21 +29,28 @@ public class JesusWay69 {
         System.out.println(spainPhoneNumberValidation("+34716984941"));
         System.out.println(urlValidation("www.retosdeprogramacion.com"));
         System.out.println(spainDniValidation("48526522Y"));
-        System.out.println(dateValidation("00-09-2050"));
+        System.out.println(dateValidation("01-09-2050"));
     }
 
     private static boolean emailValidation(String email) {
         email = email.toLowerCase();
-        return email.matches("[a-z0-9\\.]+(@)[a-z0-9]+\\.[a-z]{2,4}");
+        return email.matches("[a-z0-9\\._-]+(@)[a-z0-9]+\\.[a-z]{2,4}");
+    //Función que valida una dirección de email con usuario que puede tener letras, números, puntos
+    // o guiones, a continuación una @ , despues el proveedor que sólo puede incluir letras y números
+    // y separado por un punto el dominio que puede tener de 2 a 4 letras
     }
 
     private static boolean spainPhoneNumberValidation(String phoneNumber) {
         return phoneNumber.matches("[9||7||6][0-9]{8}||(\\+34)[9||7||6][0-9]{8}");
+   //Función que valida un nº de tlf español cuyo primer número debe ser 6,7 o 9 ,
+   //   tener en total 9 números y que puede incluir o no el prefijo español +34  
     }
 
     private static boolean urlValidation(String url) {
         url = url.toLowerCase();
-        return url.matches("[a-z0-9.]{1,63}\\.[a-z]+||(www)\\.[a-z0-9.]{1,63}\\.[a-z]+");
+        return url.matches("[a-z0-9._-]{1,63}\\.[a-z]{2,8}||(www)\\.[a-z0-9._-]{1,63}\\.[a-z]{2,8}");
+    //Función que valida una url básica con dominio de hasta 63 caracteres que pueden incluir puntos y guiones
+    //   y , separado por un punto el TLD que puede ser de entre 2 y 8 letras (por ej .business)
     }
 
     private static boolean spainDniValidation(String dniNum) {
