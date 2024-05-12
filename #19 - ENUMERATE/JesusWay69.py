ea81954ba957a20b287628f2434ca0227923c523
JesusWay69 HP elitebook <jesusway69@gmail.com>
2024-05-12 23:44:44 +0200

    20240512 Continuación ejercicio 19 Python

--- #19 - ENUMERATE/JesusWay69.py
+++ #19 - ENUMERATE/JesusWay69.py
@@ -53,18 +53,64 @@ show_day(6)
 class Orders():
     def __init__(self) -> None:
         self.orders_state = ["PENDIENTE", "ENVIADO", "ENTREGADO", "CANCELADO" ]
-    def state(self,num:int):
+    def state(self,num=1):
         self.state_order = dict(enumerate(self.orders_state,start=1))
-        return self.state_order.values(num)
+        return self.state_order.get(num)
     def waiting_to_cancelled(self):
-        pass
+        if self.state()=="PENDIENTE":
+         self.state(4)
+        else:
+            print("No se puede cancelar un producto que ya se ha enviado")
     def waiting_to_sent(self):
-        pass
+        if self.state()=="PENDIENTE":
+            self.state(2)
+        else:
+            print("No se puede enviar un producto que ya se ha enviado, entregado o cancelado")
     def sent_to_delivered(self):
-        pass
+        if self.state()=="ENVIADO":
+            self.state(3)
+        else:
+            print("No se puede marcar como entregado un producto que no se ha enviado, ya se ha entregado o se ha cancelado")
 
 orders_instance = Orders()
+
+
     
+product_dict = { "P1":1,"P2":1,"P3":1,"P4":1,"P5":1,"P6":1,"P7":1,"P8":1,"P9":1,"P10":1}
+
+while True:
+    product = int(input("Seleccione un nº de producto:"))
+    if product<1 or product>10 or product.is_integer == False:
+        print("Sólo se pueden introducir números del 1 al 10, intente de nuevo")
+    else:
+        my_product = "P"+str(product)
+        state = int(input("Elija una opción; 1-pasar a enviado 2-pasar a entregado 3-pasar a cancelado"))
+        if state <1 or state>4 or state.is_integer== False:
+             print("Sólo se pueden introducir números del 1 al 3, intente de nuevo")
+        elif state==1:
+            orders_instance.waiting_to_sent()
+            product_dict[my_product]=orders_instance.state()
+            print(f"El producto {my_product} se ha {orders_instance.state(2)}")
+            break
+        elif state==2:
+            if product_dict[my_product]!="ENVIADO":
+                print("No se puede entregar un producto que no se ha enviado")
+            else:
+                orders_instance.sent_to_delivered()
+                product_dict[my_product]=orders_instance.state()
+                print(f"El producto {my_product} se ha {orders_instance.state()}")
+                break
+
+   
+    
+    
+    
+    
+    
+    
+    
+    
+  
 
             
 
