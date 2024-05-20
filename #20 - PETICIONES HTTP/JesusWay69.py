d113b883e2ebb2abbb68410abfe71a987a9d6570
JesusWay69 HP elitebook <jesusway69@gmail.com>
2024-05-20 19:10:49 +0200

    20240520 Ejercicio 20 terminado Python

--- #20 - PETICIONES HTTP/JesusWay69.py
+++ #20 - PETICIONES HTTP/JesusWay69.py
@@ -9,16 +9,20 @@ os.system('cls')
 """
 
 try:
-    response = requests.get('https://moure.dev', timeout=1.00) 
-    print(response.text)
-except requests.exceptions.RequestException:
+    response = requests.get('https://retosdeprogramacion.com', timeout=1.00) 
+    #Con la función get de response obtenemos la respuesta de la web que le pasemos por parámetro, también le podemos pasar
+        # otros parámetros como en este caso un límite de tiempo de espera en este caso de 1 segundo.
+    print(response.text) #Despues con el método text convertimos esa respuesta en el código html de la web.
+    print(response.status_code) #Con el método status_code obtenemos el código de estado de la respuesta.
+except requests.exceptions.RequestException: #Excepción genérica para cualquier fallo en la obtención de la respuesta
     print("No se ha podido obtener la información")
-except ConnectionError:
+except ConnectionError: #Excepción de comunicación con el servidor remoto.
     print("Error de conexión")
-except TimeoutError:
-    print("Seha agotado el tiempo de espera")
+except TimeoutError: #Excepción concreta de agotamiento del tiempo de espera especificado.
+    print("Se ha agotado el tiempo de espera")
 
-""" * DIFICULTAD EXTRA (opcional):
+""" 
+ * DIFICULTAD EXTRA (opcional):
  * Utilizando la PokéAPI (https://pokeapi.co), crea un programa por
  * terminal al que le puedas solicitar información de un Pokémon concreto
  * utilizando su nombre o número.
@@ -32,7 +36,7 @@ def find():
     try:
         pokedata = get_data('https://pokeapi.co/api/v2/pokemon/'+ pokemon_name)
     except requests.RequestException as ex:
-        print ("URL NO ENCONTRADA:  ",ex)
+        print ("URL no encontrada:  ",ex)
     except Exception as name:
         print("No se ha introducido ningún parámetro",name)
     else:
@@ -41,13 +45,13 @@ def find():
         
         print('Nombre: ',pokedata['name'].capitalize())
         print('ID:     ',pokedata['id'])
-        print('Peso:   ',pokedata['weight'])
-        print('Altura: ',pokedata['height'])
+        print('Peso:   ',pokedata['weight']/10, 'Kgs')
+        print('Altura: ',pokedata['height']/10, 'm')
         print('Tipo/s:  ',', '.join(pokemon_types).title())
         print('Juegos: ',', '.join(pokemon_games).title())
         get_evolve('https://pokeapi.co/api/v2/evolution-chain/'+ str(pokedata['id']))
 
-def get_data(url=''):
+def get_data(url:str) ->dict:
     pokemon_data = {
     'name': '',
     'id':'',
@@ -68,28 +72,25 @@ def get_data(url=''):
     pokemon_data['game_indices'] = data['game_indices']
     return pokemon_data
 
-def get_evolve(url):
+def get_evolve(url:str):
 
     response = requests.get(url)
+    data = response.json()['chain'] #diccionario principal
+    evolve = data['evolves_to'] #lista de diccionarios que almacenan las evoluciones
 
-    data = response.json()['chain'] #diccionario
-
-    evolve = data['evolves_to'] #lista de 1 elemento
-
-    if (len(evolve)<1):
+    if (len(evolve) < 1):
         return
     else: 
         print("Evoluciones: ",end=' ')
         for i in range (len(evolve)):
-            evo1 = evolve[i]['species']['name']
+            evo1:str = evolve[i]['species']['name']
             print('\b',evo1.capitalize(), end=', ')
 
-    if (len(evolve[0]['evolves_to']))<1:
+    if (len(evolve[0]['evolves_to']) < 1) :
         return
     else:
-            
         for j in range (len(evolve[0]['evolves_to'])):
-            evo2 = evolve[0]['evolves_to'][j]['species']['name']
+            evo2:str = evolve[0]['evolves_to'][j]['species']['name']
             print('\b',evo2.capitalize(), end=' ')
 print()
 find()
