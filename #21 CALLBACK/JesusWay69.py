7271ebd96ca19db43b62eaa53449c49d44bec2d0
JesusWay69 HP elitebook <jesusway69@gmail.com>
2024-05-27 00:33:09 +0200

    20240526 Ejercicio 21 casi terminado Python

--- #21 CALLBACK/JesusWay69.py
+++ #21 CALLBACK/JesusWay69.py
@@ -1,4 +1,4 @@
-import os
+import os, time, random
 os.system('cls')
 
 
@@ -20,30 +20,37 @@ def sum(a,b):
     return a + b
 main_funtcion(sum, (546,7543), callback=show_result)
 
-
-
 """* DIFICULTAD EXTRA (opcional):
  * Crea un simulador de pedidos de un restaurante utilizando callbacks.
  * Estará formado por una función que procesa pedidos.
- * Debe aceptar el nombre del plato, una callback de confirmación, una
+ * Debe aceptar el nombre del food, una callback de confirmación, una
  * de listo y otra de entrega.
  * - Debe imprimir un confirmación cuando empiece el procesamiento.
  * - Debe simular un tiempo aleatorio entre 1 a 10 segundos entre
  *   procesos.
  * - Debe invocar a cada callback siguiendo un orden de procesado.
- * - Debe notificar que el plato está listo o ha sido entregado."""
-
+ * - Debe notificar que el food está listo o ha sido entregado."""
 
 def processing(state,args,callback):
     result = state(*args)
+    if result == None:
+        return
     callback(result)
 
-def confirmation():
-    pass
+def confirmation(*food):
+    print(F"El plato de {food} se ha confirmado")
+    seconds = random.randint(1,10)
+    time.sleep(seconds)
+    return ready(food)
 
-def ready():
-    pass
 
-def delivery():
-    pass
-processing()
+def ready(*food):
+    print(F"El plato de {food} está listo para servir")
+    seconds = random.randint(1,10)
+    time.sleep(seconds)
+    return delivery(food)
+
+def delivery(*food):
+    print(F"El plato de {food} está servido")
+      
+processing(confirmation, ("fabada asturiana","cocido"), callback=ready)
