4ac73ad61c23632231f2041cbc18399a98a5f32d
JesusWay69 HP elitebook <jesusway69@gmail.com>
2024-06-03 18:26:19 +0200

    20240603 Ejercicio 22 terminado Python

--- #22 - ORDEN SUPERIOR/JesusWay69.py
+++ #22 - ORDEN SUPERIOR/JesusWay69.py
@@ -8,42 +8,43 @@ from datetime import datetime as DT
  *
 """
 
-def operator(fun):
-    def sum(num1,num2):
+def operator(fun:str):#Función de orden superior
+    def sum(num1:int,num2:int)->int:#Función secundaria
         return num1+num2
-    def multiplication(num1,num2):
+    def multiplication(num1:int,num2:int)->int:#Función secundaria
         return num1*num2
-    def substraction(num1,num2):
+    def substraction(num1:int,num2:int)->int:#Función secundaria
         return num1-num2
-    def division (num1,num2):
-        if num2 !=0:
+    def division (num1:int,num2:int)->int:#Función secundaria
+        try:                                            
            return num1/num2
-        else:
-            return "No se puede dividir entre 0"
-    def pow (num1,num2):
+        except ZeroDivisionError as zde:
+            print("ERROR:",zde) 
+    def pow (num1:int,num2:int)->int:#Función secundaria
         return num1**num2
-    if fun == '+':
-        return sum
-    elif fun == '-':
-        return substraction
-    elif fun == '*':
-        return multiplication
-    elif fun == "**":
-        return pow
-    elif fun == '/':
-        return division
+    if fun == '+':#Condicional para función de orden superior
+        return sum #Retorno de resultado de función secundaria
+    elif fun == '-':#Condicional para función de orden superior
+        return substraction#Retorno de resultado de función secundaria
+    elif fun == '*':#Condicional para función de orden superior
+        return multiplication#Retorno de resultado de función secundaria
+    elif fun == "**":#Condicional para función de orden superior
+        return pow#Retorno de resultado de función secundaria
+    elif fun == '/':#Condicional para función de orden superior
+        return division#Retorno de resultado de función secundaria
     else:
         print("Operador incorrecto")
     
+print(operator('+')(6,5))#Llamada a función de orden superior asignando en los primeros paréntesis
+# su argumento y dentro del paréntesis siguiente los argumentos de la función secundaria a la que llama la primera
+resta = operator('-')#También se puede almacenar en una variable la llamada a la función de orden superior
+print(resta(456,89.5))# con su argumento y pasarle a esta variable después los argumentos de la secundaria 
 suma = operator('+')
-resta = operator('-')
 multiplicacion = operator('*')
 division = operator('/')
 potencia = operator("**")
-print (suma(485,896),"",resta(456,89.5),"",division(4,0),"",potencia(2,8),"",multiplicacion(14,24))
+print (suma(485,896),"",division(4,0),"",potencia(2,8),"",multiplicacion(14,24))
 print('\n\n\n\n')
-
-
 
 """ * DIFICULTAD EXTRA (opcional):
  * Dada una lista de estudiantes (con sus nombres, fecha de nacimiento y 
@@ -58,74 +59,82 @@ print('\n\n\n\n')
  *   de los alumnos.
  * - Una calificación debe estar comprendida entre 0 y 10 (admite decimales)."""
 
-students_list = [["Jesus",DT.strptime('26/12/1974','%d/%m/%Y'),[9.8, 8.3, 7.8, 9.9, 10, 9.7, 9.6, 9.7, 9.3]],
-                 ["Sandra",DT.strptime('24/3/1989','%d/%m/%Y'),[6.2, 9.6, 7.9, 8, 7.8, 8.4, 9, 7.8, 9.8, 8.6]],
-                 ["Pablo",DT.strptime('14/8/1982','%d/%m/%Y'),[6.5, 7.3, 8, 9.6, 5, 8.5, 9, 9.8, 8, 9.6, 9]],
-                 ["Leire",DT.strptime('2/11/1991','%d/%m/%Y'),[9.5, 9.7, 9.8, 9.3, 7.7, 9, 9, 8.7, 9.9, 8.9]]]
+students_list = [
+                 ["Ana", DT.strptime('31/1/1997', '%d/%m/%Y'), [10, 9.9, 8.8, 9.6, 8.7, 9.5, 9.2, 8.9, 9.9, 10]],
+                 ["Carlos", DT.strptime('21/3/1986', '%d/%m/%Y'), [8, 9.9, 8, 9.9, 8.7, 9.8, 9, 8.8, 9, 10, 6.9]],
+                 ["Jesus", DT.strptime('26/12/1974', '%d/%m/%Y'), [9.8, 8.3, 7.8, 9.9, 10, 9.7, 9.6, 9.7, 9.3]],
+                 ["Leire", DT.strptime('2/11/1991', '%d/%m/%Y'), [9.5, 9.7, 9.8, 9.3, 7.7, 9, 9, 8.7, 9.9, 8.9]],
+                 ["Pablo", DT.strptime('14/8/1982', '%d/%m/%Y'), [6.5, 7.3, 8, 9.6, 5, 8.5, 9, 9.8, 8, 9.6, 9]],
+                 ["Sandra", DT.strptime('24/3/1989', '%d/%m/%Y'), [6.2, 9.6, 7.9, 8, 7.8, 8.4, 9, 7.8, 9.8, 8.6]],
+                 ["Sara", DT.strptime('11/6/1991', '%d/%m/%Y'), [9.7, 9.7, 7.8, 9.9, 7.8, 9.5, 9.3, 9.7, 9.9, 10]],
+                 ["Steven", DT.strptime('10/2/1994', '%d/%m/%Y'), [9.7, 9.1, 8.8, 7.3, 7.9, 9.8, 9, 6.7, 10, 9]]
+                 ]
 
 def students(option:int):
-    def avg_qualification(students_list:list)->dict:
-        qualification_list = []
-        names_list=[]
-        acc=0
-        
+
+    def avg_grade(students_list:list)->dict:
+        grade_list = []
+        names_list = []
+        acc = 0
         for student in students_list:
             names_list.append(student[0])
-            for qualification in student[2]:
-                acc = qualification + acc
-                qualification=0
-            qualification_list.append(round(acc/len(student[2]),1))
-            acc=0
+            for grade in student[2]:
+                acc = grade + acc
+                grade = 0
+            grade_list.append(round(acc/len(student[2]),1))
+            acc = 0
         if option == 1:
             print("Nota media de los alumnos: \n")
-            for name, avg in zip(names_list,qualification_list) :
-                print("Nombre:",name," Nota media:", avg)
-        name_qualifications_dict = {name:avg for name, avg in zip(names_list,qualification_list)}     
-        return name_qualifications_dict
+            for name, avg in zip(names_list,grade_list) :
+                print('{:<10}'.format(name)," Nota media:", avg)
+        name_grades_dict = {name:avg for name, avg in zip(names_list,grade_list)}     
+        return name_grades_dict
+
 
     def best(students_list:list):
-      best_students = avg_qualification(students_list)
+      best_students = avg_grade(students_list)
       print("Estudiantes con un 9 o más de nota media: \n")
-      for k,v in best_students.items():                                                                
-          if v>=9:
-              print (k,v)
+      for name,grade_over_nine in best_students.items():                                                                
+          if grade_over_nine >= 9:
+              print ('{:<10}'.format(name), "-  Nota media:", grade_over_nine)
+
 
     def sorted_age(students_list:list):
         print("Lista de estudiantes ordenados desde el más joven:\n")
-        sorted_age_list = sorted(students_list, key=lambda student: student[1],reverse=True)
+        sorted_age_list = sorted(students_list, key=lambda student: student[1], reverse=True)
         for student in sorted_age_list:
             birth_date = student[1].strftime('%d/%m/%Y')
-            print(f"{student[0]}  -  Fecha de nacimiento: {birth_date}")
+            print('{:<10}'.format(student[0]), "-  Fecha de nacimiento:", birth_date)
     
 
-    def hight_qualification(students_list:list):
+    def hight_grade(students_list:list):
         print("Lista de estudiantes con su nota más alta: \n")
         for student in students_list:
-            hightest=0
+            hightest = 0
             name = student[0]
-            for qualification in student[2]:
-                if qualification > hightest:
-                    hightest = qualification
-            print (f"{name}  -  Calificación más alta: {hightest}")
+            for grade in student[2]:
+                if grade > hightest:
+                    hightest = grade
+            print ('{:<10}'.format(name), "-  Calificación más alta: ",hightest)
 
-    if option==1:
-        return avg_qualification
-    if option==2:
+    if option == 1:
+        return avg_grade
+    if option == 2:
         return best
-    if option==3:
+    if option == 3:
         return sorted_age
-    if option==4:
-        return hight_qualification
+    if option == 4:
+        return hight_grade
     
 while True:
-    option = input("""Elija una opción:\n1-Mostrar la nota media de todos los alumnos\n2-Mostrar los alumnos con nota media igual o superior a 9
-    3-Mostrar todos los alumnos ordenados por fecha de nacimiento desde el más joven\n4-Mostrar todos los alumnos con su nota más alta\n5-Salir\n---> """)
-    if option=="5":
+    option = input("""\nElija una opción:\n1-Mostrar la nota media de todos los alumnos\n2-Mostrar los alumnos con nota media igual o superior a 9
+3-Mostrar todos los alumnos ordenados por fecha de nacimiento desde el más joven\n4-Mostrar todos los alumnos con su nota más alta\n5-Salir\n---> """)
+    if option == "5":
         break
-    if option.isdigit:
-        option = int(option) 
-        if int(option)>4 or int(option)<1:
-            print("Solo se pueden introducir números del 1 al 5, intente de nuevo o pulse enter para salir")        
+    elif option.isdigit():
+        if int(option) > 4 or int(option) < 1:
+            print("Solo se pueden introducir números del 1 al 5, intente de nuevo o pulse 5 para salir\n") 
+            print()       
         else:
             students(int(option))(students_list)
             break
