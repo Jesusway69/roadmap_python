97435c133fd5acf1e0f4ced9afe5198420c1ecb9
Jesusway69 Mac mini macOS <jesusway69@gmail.com>
2024-07-07 13:07:40 +0200

    20240707 Continuación ejercicio 27

--- #27 - SOLID2/JesusWay69.py
+++ #27 - SOLID2/JesusWay69.py
@@ -20,11 +20,42 @@ else:
  * 3. Agrega una quinta operación para calcular potencias.
  * 4. Comprueba que se cumple el OCP."""
     
-    class Operator:
-        def __init__(self,num1, num2) -> None:
-            self.num1 = num1
-            self.num2 = num2
+class Operator:
+    def __init__(self,num1, num2) -> None:
+        self.num1 = num1
+        self.num2 = num2
 
-        def my_sum(self):
-            return self.num1 + self.num2
+    def my_sum(self):
+        return self.num1 + self.num2
+    def my_subt(self):
+        return self.num1 - self.num2
+    def my_mult(self):
+        return self.num1 * self.num2
+    def my_div(self):
+        return round((self.num1 / self.num2),2)
         
+op1 = Operator(2,3)
+
+print(op1.my_sum())
+print(op1.my_subt())
+print(op1.my_div())
+print(op1.my_mult())
+
+class Power(Operator):
+    def __init__(self,num1, num2) -> None:
+        self.num1 = num1
+        self.num2 = num2
+        
+
+    def my_pow(self):
+        acc = self.num1
+        for i in range(1, self.num2):
+            acc = Operator(self.num1 , acc).my_mult()   
+        return acc
+print()
+op1 = Power(2,3)
+print(op1.my_sum())
+print(op1.my_subt())
+print(op1.my_div())
+print(op1.my_mult())
+print(op1.my_pow())
