d44004cc701640cf7cdbcf922b05b5f20af85a2e
JesusWay69 HP elitebook <jesusway69@gmail.com>
2024-07-09 20:47:55 +0200

    20240709 Inicio ejercicio 27 Python, renombrado de carpetas #26 y #27

---
+++ #28 - SOLID LSP/JesusWay69.py
@@ -0,0 +1,95 @@
+import os, platform
+from math import pi
+from abc import ABC, abstractmethod
+if (platform.platform().startswith("macOS") or platform.platform().startswith("Linux")):
+    os.system('clear')
+else:
+    os.system('cls')
+
+""" 
+ * EJERCICIO:
+ * Explora el "Principio SOLID de Sustitución de Liskov (Liskov Substitution Principle, LSP)" 
+ * y crea un ejemplo simple donde se muestre su funcionamiento
+ * de forma correcta e incorrecta.
+ *
+"""
+
+class Figure1:
+    def __init__(self, base, height) -> None:
+        self.base = base
+        self.height = height
+    def calculate_area(self):
+        return self.base * self.height
+    
+class Rectangle1(Figure1):
+    def __init__(self, base, height) -> None:
+        super().__init__(base, height)
+    def calculate_area(self):
+        return super().calculate_area()
+
+class Square1(Figure1):
+    def __init__(self, base, height) -> None:
+        super().__init__(base, height)
+        self.height = base
+    def calculate_area(self):
+        return super().calculate_area()
+    
+rectangle1 = Rectangle1(2,5).calculate_area()
+square1 = Square1(2,5).calculate_area()
+print(rectangle1)
+print(square1)
+
+"""
+El Principio de Sustitución de Liskov establece que las subclases deben ser sustituibles por sus clases base.
+En el caso de Figure1 no se cumple este principio porque para calcular el área de un cuadrado es innecesario
+el aporte de 2 argumentos (base y altura) aunque en este caso concreto hemos "trampeado" la clase para que tome sólo
+un valor e ignore el otro, aun así siguen siendo obligatorios los 2 argumentos aunque el 2do sea innecesario, además
+al tener la misma fórmula de multiplicación simple para calcular el área el método calculate_area nos valdría pero si
+quisiéramos ampliar el programa y añadir una clase que cree objetos circulares y calcular su área ya no nos serviría
+la clase padre y llamar a su método calculate_area """
+
+
+class Figure(ABC):
+    @abstractmethod
+    def calculate_area(self):
+        pass
+
+class Rectangle2(Figure):
+    def __init__(self,base, height) -> None:
+        self.base = base
+        self.height = height
+    def calculate_area(self):
+        return self.base * self.height
+    
+class Square2(Figure):
+    def __init__(self, side) -> None:
+        self.side = side
+    def calculate_area(self):
+        return self.side ** 2
+    
+class Circle2(Figure):
+     def __init__(self, radius) -> None:
+        self.radius = radius
+     def calculate_area(self):
+         return round((self.radius ** 2 * pi),2)
+     
+rectangle2 = Rectangle2(2,5).calculate_area()
+square2 = Square2(5).calculate_area()
+circle2 = Circle2(5).calculate_area()
+print(rectangle2)
+print(square2)
+print(circle2)
+
+
+
+""" 
+ * DIFICULTAD EXTRA (opcional):
+ * Crea una jerarquía de vehículos. Todos ellos deben poder acelerar y frenar, así como
+ * cumplir el LSP.
+ * Instrucciones:
+ * 1. Crea la clase Vehículo.
+ * 2. Añade tres subclases de Vehículo.
+ * 3. Implementa las operaciones "acelerar" y "frenar" como corresponda.
+ * 4. Desarrolla un código que compruebe que se cumple el LSP."""
+
+
