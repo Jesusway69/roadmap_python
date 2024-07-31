066dfca590e533127669e5806447ac639b1cd1d5
JesusWay69 HP elitebook <jesusway69@gmail.com>
2024-07-31 19:42:38 +0200

    20240731 Ejercicio 30 terminado Python

--- #30 - SOLID DIP/JesusWay69.py
+++ #30 - SOLID DIP/JesusWay69.py
@@ -18,9 +18,10 @@ else:
 """
 El Principio de Inversión de Dependencia (DIP) establece que las clases de alto nivel
  no deben depender de clases de bajo nivel.
-Es decir, se deben fabricar abstracciones mediante interfaces y clases abstractas
- para asegurarse de que dos módulos que deben interactuar entre sí solo se comuniquen
-  a través de estas mismas, y de esta manera para esconder sus implementaciones concretas."""
+En el siguiente caso no se cumple el principio ya que hay una clase Fruta que establece la
+especie de fruta y la impresión con la lógica para que el artículo en el mensaje se adapte a
+si la especie de fruta se nombre con género masculino o femenino y a su vez las clases que crean
+objetos de esos tipos de fruta sólo setean su nombre heradando de Fruta que crea todo el trabajo"""
 
 class Fruit:
     def __init__(self, object) -> None:
@@ -50,6 +51,10 @@ class Melon(Fruit):
 pera = Fruit(Pear()).print_fruit()
 melon = Fruit(Melon()).print_fruit()
 manzana = Fruit(Apple()).print_fruit()
+
+"""En el siguiente caso creamos 2 clases abstractas, una que establece el seteo del nombre
+y otra que gestiona la impresión, las clases que generen objetos de tipos de fruta heredan
+de ambas clases abstractas , setean su nombre y sólo llaman a sus métodos que son comunes a todas la frutas."""
 
 class FruitDIP(ABC):
     @abstractmethod
@@ -88,6 +93,16 @@ naranja = Orange().print_fruit()
 sandia = WaterMelon().print_fruit()
 limon = Lemon().print_fruit()
 
+"""En Python el concepto de clase abstracta o interface es algo ambiguo, en el caso mostrado las clases
+FruitDIP y PrintFruitDIP se pueden interpretar como clases abstractas ya que aunque todos sus métodos sean
+abstractos y por lo tanto es obligatorio implementarlos en las clases que hereden de esas clases ya tienen
+su lógica y por lo tanto es más adecuado interpretarlas como clases abstractas, en otros lenguajes como Java
+la declaración de interface se hace tal cual y no permite crear lógica alguna en los métodos que se declaren dentro
+de la misma, además se pueden crear clases abstractas como tal y en este caso sí se pueden crear métodos con lógica
+y llamarlos desde las clases que hereden de la clase abstracta con la particularidad de que no permite la herencia
+múltiple de varias clases abstractas pero sí de varias interfaces, por ello Python permite mayor flexibilidad pero 
+tambien puede crear mayor confusión en estructuras de clases"""
+
     
 """ 
  * DIFICULTAD EXTRA (opcional):
@@ -114,20 +129,21 @@ class MessageInterface(ABC):
         pass
 
 class SendNotification(MessageInterface):
+
     def email(self, message):
         self.message = message
-        print (self.message, "--> Enviando email...")
+        print (self.message , "--> Enviando email...")
     def push(self,message):
         self.message = message
-        print (self.message,"--> Enviando push...")
+        print (self.message, "--> Enviando push...")
     def sms(self,message):
         self.message = message
         print (self.message,"--> Enviando SMS...")
 
-class Confirmation(MessageInterface):   
-    def confEmail(self,message):
-        message = SendNotification()
-        print (message,"--> Email enviado")
+class Confirmation(MessageInterface): 
+    def email(self, message):
+        self.message = message
+        print (self.message,"--> Email enviado")
     def push(self,message):
         self.message = message
         print (self.message,"--> Mensaje PUSH enviado")
@@ -137,18 +153,33 @@ class Confirmation(MessageInterface):
     
 class Email(SendNotification, Confirmation):
     def __init__(self):
-        message = "Esto es un mensaje de email..."
-        return super().email(message)
-class Push(SendNotification):
-    def __init__(self) -> None:
-        message = "Esto es un mensaje PUSH..."
-        return super().push(message)
-class Sms(SendNotification):
-    def __init__(self) -> None:
-        message = "Esto es un mensaje SMS..."
-        return super().sms(message)
+        self.message = "Esto es un mensaje de email..."
+        super().email(self.message)
+    def confirmation(self):
+        super(SendNotification,self).email(self.message)
+
+class Push(SendNotification, Confirmation):
+    def __init__(self):
+        self.message = "Esto es un mensaje PUSH..."
+        return super().push(self.message)
+    def confirmation(self):
+        super(SendNotification,self).push(self.message)
+    
+class Sms(SendNotification, Confirmation):
+    def __init__(self):
+        self.message = "Esto es un mensaje SMS..."
+        return super().sms(self.message)
+    def confirmation(self):
+        super(SendNotification,self).sms(self.message)
     
 email = Email()
-email.confEmail(email)
+email.confirmation()
 push = Push()
+push.confirmation()
 sms = Sms()
+sms.confirmation()
+
+
+
+
+
