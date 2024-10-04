616cdbb91b3be8d07365d86628e264dc3445164e
JesusWay69 HP elitebook <jesusway69@gmail.com>
2024-10-04 11:28:25 +0200

    20241004 Continuación ejercicio 40 Python

--- #40 - FORNITE RUBIUS/JesusWay69.py
+++ #40 - FORNITE RUBIUS/JesusWay69.py
@@ -22,8 +22,29 @@ else:
  * - Crea un ranking por número de seguidores y por antigüedad.
  * - Si algún participante no tiene usuario en Twitch, debe reflejarlo."""
 
-async def prueba():
+async def get_id(channel:str):
     client = await Twitch(credentials.client_id,credentials.secret_id)
-    user = await first(client.get_users(logins="elquintogrande"))
-    print(user.description)
-asyncio.run(prueba())
+    user = await first(client.get_users(logins=channel))
+    return user.id
+
+
+
+
+
+
+streamers_list = ["AdriContreras4", "ache", "agustin51", "alexby11" ,"ampeterby7" , "arigameplays", "arigeli_",
+                 "auronplay","axozer", "beniju03", "bycalitos", "byviruzz" ,"carreraaa" , "celopan", "crystalmolly",
+                 "darioemehache","dheylo", "djmariio", "doble", "elvisayomastercard" ,"elyas360" , "folagorlives", 
+                 "guanyar", "hika","hiperop", "ibai", "ibelky_", "illojuan" ,"imantado" , "irinaisasia", "jesskiu",
+                 "jopa","jordiwild", "kenaisouza", "keroro", "kiddkeo" ,"kikorivera" , "knekro", "kokorok", "kronnozomber",
+                 "leviathan", "littleragergirl", "lola_lolita", "lolito" ,"lolkilla" , "luzu", "mangel", "mayichi",
+                 "meelo", "missasofia", "mixwell", "mrjagger" ,"nategentile" , "nexxuz", "nissaxter", "ollie",
+                 "orslok", "outconsumer", "papigavi", "paracetamor" ,"patica" , "paulagonu", "pausenpaii", "perxita",
+                 "plex", "polispol", "quackity", "recuerdop" ,"reven" , "rivers", "robertpg", "roier",
+                 "rojuu", "rubius", "shadoune", "silithur" ,"spreen" , "spursito", "srcheeto", "staxx",
+                 "suzyrox", "thegrefg", "tvandeR", "vicens" ,"vituber" , "werlyb", "xavi", "xcrystal",
+                 "xokas", "zarcort", "zeling", "zorman"]
+
+print(asyncio.run(get_id(streamers_list[0])))
+
+
