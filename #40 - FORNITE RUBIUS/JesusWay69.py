6cb7910af9529474a3d7f7d65901d481741d3815
JesusWay69 HP elitebook <jesusway69@gmail.com>
2024-10-05 23:59:09 +0200

    20241005 Continuación ejercicio 40 Python

--- #40 - FORNITE RUBIUS/JesusWay69.py
+++ #40 - FORNITE RUBIUS/JesusWay69.py
@@ -1,8 +1,8 @@
-import os, platform, credentials
+import os, platform, credentials,asyncio, requests
 from twitch import TwitchClient
 from twitchAPI.twitch import Twitch
 from twitchAPI.helper import first
-import asyncio
+
 if (platform.platform().startswith("macOS") or platform.platform().startswith("Linux")):
     os.system('clear')
 else:
@@ -22,16 +22,29 @@ else:
  * - Crea un ranking por número de seguidores y por antigüedad.
  * - Si algún participante no tiene usuario en Twitch, debe reflejarlo."""
 
-async def get_id(channel:str):
-    client = await Twitch(credentials.client_id,credentials.secret_id)
-    user = await first(client.get_users(logins=channel))
-    return user.id
 
+client_id = credentials.client_id
+secret_id = credentials.secret_id
 
+def get_token(client_id, client_secret):
+    url = 'https://id.twitch.tv/oauth2/token'
+    
+    data = {
+        'client_id': client_id,
+        'client_secret': client_secret,
+        'grant_type': 'client_credentials'
+    }
 
+    response = requests.post(url, data=data)
 
+    if response.status_code != 200:
+        raise Exception('Error al obtener el token OAuth')
 
+    result = response.json()
+    return result['access_token']
 
+token = get_token(client_id, secret_id)
+print (token)
 streamers_list = ["AdriContreras4", "ache", "agustin51", "alexby11" ,"ampeterby7" , "arigameplays", "arigeli_",
                  "auronplay","axozer", "beniju03", "bycalitos", "byviruzz" ,"carreraaa" , "celopan", "crystalmolly",
                  "darioemehache","dheylo", "djmariio", "doble", "elvisayomastercard" ,"elyas360" , "folagorlives", 
@@ -45,6 +58,29 @@ streamers_list = ["AdriContreras4", "ache", "agustin51", "alexby11" ,"ampeterby7
                  "suzyrox", "thegrefg", "tvandeR", "vicens" ,"vituber" , "werlyb", "xavi", "xcrystal",
                  "xokas", "zarcort", "zeling", "zorman"]
 
-print(asyncio.run(get_id(streamers_list[0])))
+
+
+async def get_id(channel:str):
+    client = await Twitch(client_id,secret_id)
+    user = await first(client.get_users(logins=channel))
+    return user.id
+id = asyncio.run(get_id(streamers_list[1]))
+
+print(id)
+
+
+
+url = f'https://api.twitch.tv/helix/users/follows?broadcaster_id={id}'
+
+response = requests.get(url, headers=client)
+
+data = response.json()
+print(data)
+
+#follower_count = data['total']
+
+
+#print(f'Follower count: {follower_count}')
+
 
 
