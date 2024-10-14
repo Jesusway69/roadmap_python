5d86328f37dee6e9a00911e041d7d1e72599cb2a
JesusWay69 HP elitebook <jesusway69@gmail.com>
2024-10-14 22:00:00 +0200

    20241014 Ejercicio 41 casi terminado Python

--- #41 - ZIP/JesusWay69.py
+++ #41 - ZIP/JesusWay69.py
@@ -19,12 +19,62 @@ else:
 #         file.write(f"Este es el fichero nº {i+1}")# cuando termine el bloque de código tabulado.
 
 current_datetime = datetime.today().strftime("%Y_%m_%d_%H_%M_%S")
-backup = "#41 - ZIP/files"
-zip_path = f"#41 - ZIP/files/python_zip{current_datetime}.zip"
-os.chdir(backup)
-files_folder = os.listdir("./")
-zip_file = zipfile.ZipFile(zip_path, "w")
 
-for file in files_folder:
-    zip_file.write(backup+file, compress_type=zipfile.ZIP_DEFLATED)
-zip_file.close()
+absolute = r"C:\Users\jesus\Documents\Python3project\roadmap_python\#41 - ZIP\files\\"
+
+zip_path = "#41 - ZIP/python_zip" + current_datetime + ".zip"
+
+
+zip = zipfile.ZipFile(zip_path, mode= 'w') 
+
+for file in os.listdir(absolute):
+    zip.write(os.path.join(absolute, file))
+zip.close()
+
+#crea el zip con todo el directorio absoluto dentro
+
+
+
+
+
+
+
+
+
+
+# os.chdir(backup)
+
+# files_folder = os.listdir("./")
+
+# zip_file = zipfile.ZipFile(zip_path, "w")
+# print(files_folder)
+# for file in files_folder:
+#     zip_file.write(filename=file, arcname=backup, compress_type=zipfile.ZIP_DEFLATED)
+# zip_file.close()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# current_datetime = datetime.today().strftime("%Y_%m_%d_%H_%M_%S")
+# backup = "#41 - ZIP/files/"
+# zip_path = "#41 - ZIP/files/python_zip" + current_datetime + ".zip"
+# os.chdir(backup)
+# files_folder = os.listdir("./")
+# with zipfile.ZipFile(zip_path, "w", zipfile.ZIP_DEFLATED) as zip_file:
+
+#     for root, dir, files in os.walk(backup):
+#         for file in file:
+#             path = os.path.join(root, file)
+#             zip_file.write(backup+file, compress_type=zipfile.ZIP_DEFLATED)
