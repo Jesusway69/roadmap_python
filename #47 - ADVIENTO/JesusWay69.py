17112ebd7f844c658606a17dd7d02821a4caa197
JesusWay69 HP elitebook <jesusway69@gmail.com>
2024-12-02 21:55:58 +0100

    20241202 Ejercicio 47 terminado Python

--- #47 - ADVIENTO/JesusWay69.py
+++ #47 - ADVIENTO/JesusWay69.py
@@ -31,29 +31,49 @@ else:
  *
  * Ejemplo de selección del día 1
  * **** **** **** 
- * **** *02* *03*
+ * **** *02* *03* ...
  * **** **** ****
  *
  * - Si se selecciona un número ya descubierto, se le notifica
  *   al usuario."""
 
-# stars_list = ['*','*','*','*']
+calendar = []
+used_numbers = set()
+for i in range(6):
+            calendar.append("****")
 
-# def print_stars():
-#     for star in stars_list:
-#         print(star, end='')
-# for i in range(12):
-#     print('')
-#     for j in range(6):
-#         print_stars(), print(end=' ')
+for i in range(1, 25):
+    day:str = f"*{str(i).zfill(2)}*"
+    calendar.append(day)
+    if i%6==0:
+        for i in range(6):
+            calendar.append("****")
 
-for i in range (1,349):
-    if i%5 == 0:
-        print(' ', end='')
-        i+=1
-    if i%30==0:
-        print('')
+def show_calendar(calendar:list):
+    for i in range (1,len(calendar)+1):
+        print(calendar[i-1], end=' ')
+        if i % 6 == 0:
+            print()
+    
+while True:
+    show_calendar(calendar)
+    number:str = input("\nIntroduzca el día del calendario del 1 al 24 o enter para salir: ")
+    if number == '':
+         break
+
+    elif not number.isdigit or int(number) > 24 or int(number) < 1:
+        print("El dato debe ser numérico entre 1 y 24, intente de nuevo")
+        continue
+    
     else:
-        print(i, end='')
-
-
+        target:str = number.zfill(2)
+        for number in used_numbers:
+            if number == target:
+                print(f"El número {target} ya se ha seleccionado antes, intente con otro") 
+            continue  
+        for j in range(len(calendar)):
+            if target in calendar[j]:
+                calendar[j] = "****"
+                used_numbers.add(target)
+          
+        
