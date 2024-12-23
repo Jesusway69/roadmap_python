c20e9b6d5e6975b701fd5fca7e869333cf0d7c73
JesusWay69 HP elitebook <jesusway69@gmail.com>
2024-12-23 21:37:38 +0100

    20241223 Ejercicio 48 terminado Java

--- #48 - ARBOL DE NAVIDAD/JesusWay69.java
+++ #48 - ARBOL DE NAVIDAD/JesusWay69.java
@@ -1,22 +1,20 @@
 package reto_49;
 
 import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 public class JesusWay69 {
 
-    public static List<Integer> coordinates = new ArrayList<>();
+    public static ArrayList<Integer> coordinates = new ArrayList<>();
 
     public static void main(String[] args) {
 
         char[][] myTree = null;
 
         do {
-
             Scanner sc = new Scanner(System.in);
             System.out.println("""
-              1- Crear árbol
+              1- Crear nuevo árbol
               2- Añadir estrella 
               3- Eliminar estrella 
               4- Añadir 2 bolas aleatoriamente
@@ -25,7 +23,7 @@ public class JesusWay69 {
               7- Encender las luces
               8- Apagar las luces
                              """);
-            System.out.print("Elija una opción: ");
+            System.out.print("Elija una opción (enter para salir): ");
             String option = sc.nextLine();
             if (option.equals(""))  break;
 
@@ -36,36 +34,19 @@ public class JesusWay69 {
                     myTree = createTree(hight);
                     showTree(myTree);
                 }
-                case "2" -> 
-                    showTree(topStar(myTree, true));
-                
-                case "3" -> 
-                    showTree(topStar(myTree, false));
-                
-                case "4" -> 
-                    showTree(addBalls(myTree));
-                
-                case "5" -> 
-                    showTree(removeBalls(myTree));
-                
-                case "6" -> 
-                    showTree(addLights(myTree));
-                
-                case "7" -> 
-                    showTree(turnOnLights(myTree, coordinates));
-                
-                case "8" ->
-                    showTree(turnOffLigths(myTree));
-
-                default -> 
-                    System.out.println("El dato tiene que ser numérico del 1 al 8");
-                
+                case "2" -> showTree(topStar(myTree, true));               
+                case "3" -> showTree(topStar(myTree, false));               
+                case "4" -> showTree(addBalls(myTree));              
+                case "5" -> showTree(removeBalls(myTree));               
+                case "6" -> showTree(addLights(myTree));           
+                case "7" -> showTree(turnOnLights(myTree, coordinates));              
+                case "8" ->showTree(turnOffLigths(myTree));
+                default -> System.out.println("El dato tiene que ser numérico del 1 al 8\n");               
             }
-            
         } while (true);
     }
 
-    public static List<Integer> storageCoordinates(int x, int y) {
+    public static ArrayList<Integer> storageCoordinates(int x, int y) {
         coordinates.add(x);
         coordinates.add(y);
         return coordinates;
@@ -81,48 +62,29 @@ public class JesusWay69 {
             tree = null;
             return tree;
         }
-
         for (int i = 0; i < files; i++) {
-            for (int j = 0; j < columns / 2; j++) {
-                tree[i][j] = ' ';
-            }
-            for (int k = columns / 2; k < columns / 2 + branch; k++) {
-                tree[i][k] = '*';
-            }
-            for (int l = columns / 2 + branch; l < files * 2 - 1; l++) {
-                tree[i][l] = ' ';
-            }
+            for (int j = 0; j < columns / 2; j++) tree[i][j] = ' '; 
+            for (int k = columns / 2; k < columns / 2 + branch; k++) tree[i][k] = '*';
+            for (int l = columns / 2 + branch; l < files * 2 - 1; l++) tree[i][l] = ' ';
             columns -= 2;
             branch += 2;
         }
-        columns = files * 2 - 1;
         for (int m = 0; m < 2; m++) {
-            for (int n = 0; n < files - 2; n++) {
-                tree[files + m][n] = ' ';
-            }
-            for (int p = files - 2; p < files + 1; p++) {
-                tree[files + m][p] = '|';
-            }
-            for (int q = files + 1; q < files * 2 - 1; q++) {
-                tree[files + m][q] = ' ';
-            }
+            for (int n = 0; n < files - 2; n++) tree[files + m][n] = ' ';
+            for (int p = files - 2; p < files + 1; p++) tree[files + m][p] = '|';
+            for (int q = files + 1; q < files * 2 - 1; q++) tree[files + m][q] = ' ';   
         }
         return tree;
     }
-
     public static void showTree(char[][] tree) {
         if (tree != null) {
             int i = 0;
             for (char[] branch : tree) {
                 i++;
-                for (char c : branch) {
-                    System.out.print(c);
-                }
+                for (char c : branch) System.out.print(c);
                 System.out.println();
-
             }
         }
-
     }
 
     public static char[][] topStar(char[][] tree, boolean switchStar) {
@@ -147,11 +109,8 @@ public class JesusWay69 {
         for (int i = 0; i < 2; i++) {
             int randomFile = (int) (Math.random() * tree.length - 2 + 1);
             int randomColumn = (int) (Math.random() * tree[0].length);
-            if (tree[randomFile][randomColumn] == '*') {
-                tree[randomFile][randomColumn] = 'O';
-            } else {
-                i--;
-            }
+            if (tree[randomFile][randomColumn] == '*') tree[randomFile][randomColumn] = 'O';      
+            else i--;   
         }
         System.out.println("\n--- SE AÑADEN 2 BOLAS DE ADORNO ---");
         return tree;
@@ -164,9 +123,7 @@ public class JesusWay69 {
         }
         for (int i = 1; i < tree.length - 2; i++) {
             for (int j = 0; j < tree[i].length; j++) {
-                if (tree[i][j] == 'O') {
-                    tree[i][j] = '*';
-                }
+                if (tree[i][j] == 'O') tree[i][j] = '*';      
             }
         }
         System.out.println("\n--- BOLAS DE ADORNO ELIMINADAS ---");
@@ -184,15 +141,13 @@ public class JesusWay69 {
             if (tree[randomFile][randomColumn] == '*') {
                 tree[randomFile][randomColumn] = 'X';
                 storageCoordinates(randomFile, randomColumn);
-            } else {
-                i--;
-            }
+            } else i--;   
         }
-        System.out.println("\\n--- AÑADIDAS 3 LUCES ENCENDIDAS AL ÁRBOL ---");
+        System.out.println("\n--- AÑADIDAS 3 LUCES ENCENDIDAS AL ÁRBOL ---");
         return tree;
     }
 
-    public static char[][] turnOnLights(char[][] tree, List<Integer> coordinates) {
+    public static char[][] turnOnLights(char[][] tree, ArrayList<Integer> coordinates) {
         if (tree == null) {
             System.out.println("ERROR: Hay que crear un árbol antes de poder modificarlo (opción 1)");
             return null;
@@ -217,12 +172,10 @@ public class JesusWay69 {
         }
         for (int i = 1; i < tree.length - 2; i++) {
             for (int j = 0; j < tree[i].length; j++) {
-                if (tree[i][j] == 'X') {
-                    tree[i][j] = '*';
-                }
+                if (tree[i][j] == 'X') tree[i][j] = '*';   
             }
         }
-        System.out.println("\\n--- TODAS LAS LUCES APAGADAS ---");
+        System.out.println("\n--- TODAS LAS LUCES APAGADAS ---");
         return tree;
     }
 }
