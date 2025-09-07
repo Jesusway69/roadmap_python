c1c68f4ea55e14f38e4638651dc0fc18c140e534
Jesusway69 Mac mini macOS <jesusway69@gmail.com>
2025-09-07 02:08:08 +0200

    20250907 Continuación ejercicio 46, añadido conteo de followers de cada usuario

--- #46 - X VS BLUESKY/JesusWay69.py
+++ #46 - X VS BLUESKY/JesusWay69.py
@@ -43,7 +43,6 @@ class User:
         User.auto_increment_user_id += 1
         self.id = User.auto_increment_user_id
         self.user_posts = {}
-        self.user_post_liked = []
         self.index_message = 0
         self.follow = set()
         self.user_followers = []
@@ -53,7 +52,6 @@ class User:
             print("No se puede crear un post de más de 200 caracteres")
         else:
             self.index_message +=1
-            #self.user_posts[self.index_message] = (message, DT.now())
             self.user_posts[self.index_message] = {
     "message": message,
     "date": DT.now(),
@@ -61,23 +59,28 @@ class User:
 }
 
     def following(self, *users):
-        self.follow.update(users) # type: ignore
+        for user in users:
+            self.follow.add(user)
+            if self not in user.user_followers:
+                user.user_followers.append(self)
         return self.follow
     
-    def unfollow(self, user):
-        
+    
+    def unfollow(self, user):  
         if user in self.follow:
             self.follow.remove(user) # type: ignore
+            if self in user.user_followers:
+                user.user_followers.remove(self)
             print(f"{self.name} ha dejado de seguir a {user.name}")
         else:
             print(f"{self.name} no puede dejar de seguir a {user.name} porque no lo estaba siguiendo anteriormente")
 
+
     def show_posts(self):    
+        if len(self.user_posts) == 0:
+                print(f"{self.name} no tiene nigún mensaje")
+                return
         print(f"Mensajes de {self.name}:")
-        # for k, (message, date) in reversed(self.user_posts.items()):
-        #     likes_counter = sum(1 for like_sublist in self.user_post_liked if like_sublist[1] == k)
-        #     print("-", message, ", creado el", '{}/{}/{} {}:{}:{}'.format(date.day,date.month,date.year, date.hour, date.minute, date.second),
-        #             "message id:", k , "número de likes:", likes_counter)
         for key, value in reversed(self.user_posts.items()):
             likes_counter = len(value["likes"])
             date = value["date"]
@@ -85,21 +88,25 @@ class User:
                   f"{date.day}/{date.month}/{date.year} {date.hour}:{date.minute}:{date.second}",
                   "message id:", key , "número de likes:", likes_counter)
         
+
     def show_user_profile(self):
         users_following = self.follow
         print()
-        print(f"INFORMACIÓN DE USUARIO\n -------------------\nUsername: {self.name}\nUserID: {self.id}\nSiguiendo a:")
+        print(f"INFORMACIÓN DE USUARIO\n -------------------\nUsername: {self.name}\nUserID: {self.id}\nFollowers: {len(self.follow)}\
+              \nSiguiendo a:")
         for follower in users_following:
             print("-", follower.name)
         self.show_posts()    
         print()
         
+
     def delete_post(self, messageID):
         if messageID not in self.user_posts:
             print(f"El usuario {self.name} no tiene ningún mensaje con id {messageID}")
         else:
             del(self.user_posts[messageID])
     
+
     def liked_post(self, user, postId: int):
         print("---------------------------\n")
 
@@ -109,13 +116,10 @@ class User:
 
         message = user.user_posts[postId]["message"]
 
-        for like_sublist in user.user_post_liked:
-            if like_sublist[0] == self.name and like_sublist[1] == postId and like_sublist[2] == user.name:
-                print(f"{self.name} no puede dar like al mensaje '{message}' porque ya le dio like antes\n")
-                return
+        if postId not in user.user_posts:
+            print(f"{self.name} no puede dar like al mensaje '{message}' porque ya le dio like antes\n")
+            return
 
-        user.user_post_liked.append([self.name, postId, user.name, message])
-        #likes_counter = sum(1 for like in user.user_post_liked if like[3] == message)
         user.user_posts[postId]["likes"].add(self)
         likes_counter = len(user.user_posts[postId]["likes"])
        
@@ -128,22 +132,21 @@ class User:
         if postId not in user.user_posts:
             print(f"el usuario {user.name} no tiene ningún mensaje con id {postId}\n")
             return
-        like_found = False
+        
         message = user.user_posts[postId]["message"]
-        for like_sublist in user.user_post_liked:
-            if like_sublist[0] == self.name and like_sublist[1] == postId and like_sublist[2] == user.name:
-                like_found = True
-                user.user_post_liked.remove(like_sublist)
-                #likes_counter = sum(1 for sub_list in user.user_post_liked for element in sub_list if element == message)
-                self.user_posts[postId]["likes"].discard(self)
-                likes_counter = len(user.user_posts[postId]["likes"])
-                print(f"{self.name} ha quitado el like al mensaje con id:{postId} de {user.name}"
-                f", el mensaje de {user.name} '{message}' acumula {likes_counter} {'like' if likes_counter == 1 else 'likes'}\n")
-                return
-        if not like_found:
+
+        if self not in user.user_posts[postId]["likes"]:
             print(f"{self.name} no puede quitar el like del mensaje con id {postId} de {user.name}",
                   "porque no le había dado like antes\n")
-            
+            return
+        
+        user.user_posts[postId]["likes"].discard(self)
+        likes_counter = len(user.user_posts[postId]["likes"])
+
+        print(f"{self.name} ha quitado el like al mensaje con id:{postId} de {user.name}"
+        f", el mensaje de {user.name} '{message}' acumula {likes_counter} {'like' if likes_counter == 1 else 'likes'}\n")
+        return
+        
 #LISTA DE USUARIOS
 users_list = ["Jesus", "Sara", "Luis", "Ana", "Kevin", "Sandra", "Pedro", "Megan", "Victor", "Paula",
                "Miguel", "Silvia", "Pablo", "Rocío", "Joseph", "Isabel", "Tony", "Cristina", "Marco", "Elena"]
@@ -195,8 +198,10 @@ jesus.show_user_profile()
 isabel.unfollow(jesus)
 
 #LA USUARIA ISABEL DEJA DE SEGUIR A VICTOR
+victor.show_user_profile()
 isabel.unfollow(victor)
 isabel.show_user_profile()
+victor.show_user_profile()               #arreglar bug, no bajan los seguidores
 
 #CREACIÓN DE VARIOS LIKES A MENSAJES CONCRETOS POR SU ID
 cristina.liked_post(jesus, 1)
